1c094f97fbdffe8ef1a74157ee66c7cd34678fa2
Brian Jensen <hallo@magnify.dk>
2014-02-18 10:13:01 +0100

    Added padding top to logo and removed secondary links

--- themes/mso/templates/page.tpl.php
+++ themes/mso/templates/page.tpl.php
@@ -89,25 +89,7 @@
 ?>
 <div id="page-wrapper"><div id="page">
 
-  <header id="header" role="banner" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
-    <?php if ($secondary_menu): ?>
-      <nav id="secondary-menu" role="navigation" class="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'id' => 'secondary-menu-links',
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Secondary menu'),
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav> <!-- /#secondary-menu -->
-    <?php endif; ?>
-    
-    
+  <header id="header" role="banner" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">       
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
