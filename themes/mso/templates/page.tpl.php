2b7d154095e860d82da110acd20f91ca5049593f
Brian Jensen <hallo@magnify.dk>
2014-02-18 10:37:56 +0100

    Changed font to Arial

--- themes/mso/templates/page.tpl.php
+++ themes/mso/templates/page.tpl.php
@@ -89,7 +89,7 @@
 ?>
 <div id="page-wrapper"><div id="page">
 
-  <header id="header" role="banner" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">       
+  <header id="header" role="banner" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
